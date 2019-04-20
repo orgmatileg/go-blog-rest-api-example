011c73bab35010965441ef0cc78e104573f28436
Luqmanul Hakim <arlhba@gmail.com>
2019-04-20 17:00:15 +0700

    cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,7 +6,9 @@ import "net/http"
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		next.ServeHTTP(w, r)
 	})
